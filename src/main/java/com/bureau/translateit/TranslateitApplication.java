bab7c68afaeb7ec65521df98a79433a6479529c4
Henry Thuler <henrythuler2003@outlook.com>
2025-04-23 15:44:26 -0300

    build: initial structure

---
+++ src/main/java/com/bureau/translateit/TranslateitApplication.java
@@ -0,0 +1,13 @@
+package com.bureau.translateit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslateitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslateitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bureau/translateit/TranslateitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bureau.translateit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslateitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
